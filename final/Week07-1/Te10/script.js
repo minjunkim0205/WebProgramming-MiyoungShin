6ad9ce57182a5a249ed7f17a482b82ecfb5f7f9c
minjunkim0205 <minjunkim0205@gmail.com>
2024-05-01 20:46:11 +0900

    Udpate Week09-1

--- final/Week07-1/Te10/script.js
+++ final/Week07-1/Te10/script.js
@@ -1,18 +1,20 @@
 /*Function*/
-function avgOverPass(..._users){
-    _users.map(_user => {
-        const _result = _user.ave >= 60 ? "합격" : "불합격";
-        return {..._user, _result};
-    });
-    return _users;
+function avgOverPass(_users){
+    let temp_users = _users.map(_user => ({..._user,result: _user.ave >= 60 ? '합격' : '불합격'}));
+    console.log(`${_users}`);
+    console.log(`${temp_users}`);
+    for(let e in temp_users){
+        console.log(`{ave: ${e.ave}}, name: ${e.name}, result: ${e.result}`);
+    }
+    return temp_users;
 }
 /*Main*/
 function main() {
     const users = [
-        { ave: 85, name: 'Lee' },
-        { ave: 59, name: 'Kim' },
-        { ave: 90, name: 'Choi' },
-        { ave: 50, name: 'Park' }
+        {ave:85, name:"Lee"},
+        {ave:59, name:"Kim"},
+        {ave:90, name:"Choi"},
+        {ave:50, name:"Park"}
     ];
-    console.log(`${avgOverPass(users)}`);
+    avgOverPass(users);
 }
--- final/Week07-1/Te11/script.js
+++ final/Week07-1/Te11/script.js
@@ -1,16 +1,16 @@
 /*Function*/
-function avg(..._users){
+function getAvg(..._users){
     let sum = _users.reduce((_acc, _cur) => _acc + _cur, 0);
     return (sum / _users.length);
 }
-function avgOver(..._users) {
-    let avg = avg(_users);
+function getAvgOver(..._users) {
+    let avg = getAvg(..._users); // _users 대신 ..._users 를 넣는 이유가 궁금함.
     return (_users.filter(_num => _num > avg).length);
 }
 /*Main*/
 function main() {
-    console.log(`평균 = ${avg(4, 7, 6, 12, 7, 21, 24, 75)}`);
-    //console.log(`평균이상은 모두 ${avgOver(4, 7, 6, 12, 7, 21, 24, 75)}개 입니다.`);
-    console.log(`평균 = ${avg(80, 87, 94, 92, 98, 86, 78, 90, 92)}`);
-    //console.log(`평균이상은 모두 ${avgOver(80, 87, 94, 92, 98, 86, 78, 90, 92)}개 입니다.`);
+    console.log(`평균 = ${getAvg(4, 7, 6, 12, 7, 21, 24, 75)}`);
+    console.log(`평균이상은 모두 ${getAvgOver(4, 7, 6, 12, 7, 21, 24, 75)}개 입니다.`);
+    console.log(`평균 = ${getAvg(80, 87, 94, 92, 98, 86, 78, 90, 92)}`);
+    console.log(`평균이상은 모두 ${getAvgOver(80, 87, 94, 92, 98, 86, 78, 90, 92)}개 입니다.`);
 }
